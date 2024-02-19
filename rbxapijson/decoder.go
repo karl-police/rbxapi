9627851686bfe3bf26581f2195a278efbe4179c1
karl-police <karl-police2001@bluewin.ch>
2024-02-19 22:17:30 +0100

    Update decoder.go

--- rbxapijson/decoder.go
+++ rbxapijson/decoder.go
@@ -29,6 +29,7 @@ func (root *Root) UnmarshalJSON(b []byte) (err error) {
 	if err := json.Unmarshal(b, &v); err != nil {
 		return err
 	}
+	fmt.Println("Hello")
 	switch v.Version {
 	case 1:
 		r := struct {
